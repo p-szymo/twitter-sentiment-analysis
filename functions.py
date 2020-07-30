a46ad651bbc27991332c5ec9260b85f9f9329359
“p-szymo” <joshua.szymanowski@gmail.com>
2020-07-30 11:08:09 -0400

    update gitignore, standardize documentation, add charts

--- functions.py
+++ functions.py
@@ -1,51 +1,68 @@
 # dataframe libraries
+from nltk.stem import WordNetLemmatizer
+import matplotlib.pyplot as plt
+import itertools
+import emoji
+import string
+import re
+from textblob import TextBlob
+from nltk.corpus import stopwords
+from nltk import RegexpTokenizer
+import nltk
 import numpy as np
 import pandas as pd
 
 # twitter scraper
 import twint
+
 # fixes runtime errors with twint
 import nest_asyncio
 nest_asyncio.apply()
 
-# text processing
-import nltk
-from nltk import RegexpTokenizer
-from nltk.stem import WordNetLemmatizer 
-from nltk.corpus import stopwords
-from textblob import TextBlob
-import re
-import string
-import emoji
-
-# iterate over dataframes
-import itertools
-
-# visualization library
-import matplotlib.pyplot as plt
-
 
 # twitter scrape
-def twint_search(search, username=None, since=None, until=None, drop_cols=None, limit=None):
-    
+def twint_search(
+        search,
+        username=None,
+        since=None,
+        until=None,
+        drop_cols=None,
+        limit=None):
     '''
-    Function to return a Pandas DataFrame of tweets in English containing search terms using twint.
-    
-    Input a search term as a string. NOTE: use of 'OR', 'AND', and other search commands is supported.
-    
-    Optional inputs include:
-                username: Twitter handle
-                since: start date of search
-                until: end date of search
-                drop_cols: columns of tweet data to drop
-                limit: maximum number of tweets to scrape
-                
-    Output a Pandas DataFrame.
+    Function to return a Pandas DataFrame of tweets in English containing
+    search terms using twint.
+
+    Input
+    -----
+    search : str
+        Desired search term(s).
+        Use of 'OR', 'AND', and other search commands is supported.
+
+    Optional input
+    --------------
+    username : str
+        Twitter handle.
+        `@` symbol not required.
+    since : str
+        Start date of search.
+        format ==> Y%%%-M%-D%, e.g. `2020-02-29`
+    until : str
+        End date of search (not inclusive).
+        format ==> Y%%%-M%-D%, e.g. `2020-03-01`
+    drop_cols : list (str)
+        Tweet attributes to drop.
+        For a full list of attributes, visit:
+        `https://github.com/twintproject/twint/wiki/Tweet-attributes`
+    limit : int
+        Maximum number of tweets to scrape.
+
+    Output
+    ------
+    df : Pandas DataFrame
     '''
-    
+
     # instantiate twint object
     c = twint.Config()
-    
     # set parameters
     c.Lang = 'en'
     c.Search = search
@@ -54,81 +71,119 @@ def twint_search(search, username=None, since=None, until=None, drop_cols=None, 
     c.Until = until
     c.Limit = limit
     c.Pandas = True
-    
+
     # hide the printing of every tweet during scrape
     c.Hide_output = True
-    
+
     # scrape
     twint.run.Search(c)
-    
+
     # store as pandas dataframe
     df = twint.storage.panda.Tweets_df
-    
+
     # transform date string into datetime object
     df['date'] = pd.to_datetime(df['date']).dt.date
-    
+
     return df
 
 
 # looping twitter scrape and creating dataframe
-def search_loop(start_date, end_date, search, filename, username=None, drop_cols=None, limit=None):
-    
+def search_loop(
+        start_date,
+        end_date,
+        search,
+        filename,
+        username=None,
+        drop_cols=None,
+        limit=None):
     '''
-    Function to loop over date range and perform twint_search function for each day, returning one combined dataframe.
+    Function to loop over date range and perform twint_search function for 
+    each day, returning one combined dataframe.
+    
     Periodically saves progress to CSV after each daily search.
-    
-    Input a start date, end date, search time, and file name (where to save CSV file).
-    
-    Optional inputs include:
-                username: Twitter handle
-                drop_cols: columns of tweet data to drop
-                limit: maximum number of tweets to scrape
-                
-    Output a Pandas DataFrame. Also saves as CSV.
+
+    Input
+    -----
+    start_date : str
+        Start date of search.
+        format ==> Y%%%-M%-D%, e.g. `2020-02-29`
+    end_date : str
+        End date of search (not inclusive).
+        format ==> Y%%%-M%-D%, e.g. `2020-03-01`
+    search : str
+        Desired search term(s).
+        Use of 'OR', 'AND', and other search commands is supported.
+    filename : str
+        Path and name for saved csv file.
+
+    Optional inputs
+    ---------------
+    username : str
+        Twitter handle.
+        `@` symbol not required.
+    drop_cols : list (str)
+        Tweet attributes to drop.
+        For a full list of attributes, visit:
+        `https://github.com/twintproject/twint/wiki/Tweet-attributes`
+    limit : int
+        Maximum number of tweets to scrape.
+
+    Output
+    ------
+    df : Pandas DataFrame
+        Also saves and updates a CSV file after each day in the search.
     '''
-    
+
     # instantiate empty dataframe
     df = pd.DataFrame()
-    
+
     # create panda series of desired dates
     date_range = pd.Series(pd.date_range(start_date, end_date))
-    
+
     # loop over dates
     for d in range(len(date_range) - 1):
-        
+
         # obtain target date
         since = date_range[d].strftime('%Y-%m-%d')
-        
+
         # obtain stop date
         until = date_range[d + 1].strftime('%Y-%m-%d')
-        
+
         # scrape date to temporary dataframe
-        day_df = twint_search(search=search, username=username, since=since, until=until, drop_cols=drop_cols, limit=limit)
-        
+        day_df = twint_search(
+            search=search,
+            username=username,
+            since=since,
+            until=until,
+            drop_cols=drop_cols,
+            limit=limit)
+
         # drop empty columns
         day_df.drop(columns=drop_cols, axis=1, inplace=True)
-        
-        # add new daily data to dataframe, delete temporary dataframe, reset index, save to CSV
+
+        # add new daily data to dataframe, delete temporary dataframe, reset
+        # index, save to CSV
         df = pd.concat([df, day_df])
         del day_df
         df.reset_index(drop=True, inplace=True)
         df.to_csv(f'Datasets/{filename}.csv')
-        
+
         # notification of successful scrape
         print(datetime.now(), f'{since} Saved!')
-        
+
     return df
 
 
 # emoticons
 def load_dict_emoticons():
-    
     '''
     Load a dictionary of emoticons as keys and their word equivalents as values.
-    
-    Source: https://towardsdatascience.com/twitter-sentiment-analysis-using-fasttext-9ccd04465597    
+
+    Source
+    ------
+    https://towardsdatascience.com/twitter-sentiment-analysis-using-fasttext-9ccd04465597
     '''
-    
+
     return {
         ":‑)": "smiley",
         ":-]": "smiley",
@@ -186,17 +241,19 @@ def load_dict_emoticons():
         ":þ": "playful",
         ":b": "playful",
         "<3": "love"
-        }
+    }
 
 
 # self defined contractions
 def load_dict_contractions():
     '''
     Load a dictionary of contractions as keys and their expanded words as values.
-    
-    Source: https://towardsdatascience.com/twitter-sentiment-analysis-using-fasttext-9ccd04465597  
+
+    Source
+    ------
+    https://towardsdatascience.com/twitter-sentiment-analysis-using-fasttext-9ccd04465597
     '''
-    
+
     return {
         "ain't": "is not",
         "amn't": "am not",
@@ -250,7 +307,7 @@ def load_dict_contractions():
         "it'll": "it will",
         "it'll've": "it will have",
         "it's": "it is",
-        "i've": "i have",        
+        "i've": "i have",
         "kinda": "kind of",
         "let's": "let us",
         "ma'am": "madam",
@@ -270,7 +327,7 @@ def load_dict_contractions():
         "o'er": "over",
         "ol'": "old",
         "oughtn't": "ought not",
-        "oughtn't've": "ought not have",                
+        "oughtn't've": "ought not have",
         "shalln't": "shall not",
         "shan't": "shall not",
         "shan't've": "shall not have",
@@ -364,106 +421,129 @@ def load_dict_contractions():
         "Whatcha": "What are you",
         "luv": "love",
         "sux": "sucks"
-        }
+    }
 
 
 # apply text cleaning techniques
 def clean_text(text, stop_words):
-    
     '''
-    Input text (a tweet) as a string and a list of stop words.
-    
-    Make text lowercase, remove mentions, remove links, convert emoticons and emojis to words, remove punctuation
-    (except apostrophes), tokenize words (including contractions), convert contractions to full words,
-    remove stop words.
-    
-    Output processed text as a string.
+    Function to make tweets lowercase, remove mentions, remove links, 
+    convert emoticons and emojis to words, remove punctuation (except 
+    apostrophes), tokenize words (including contractions), convert 
+    contractions to full words, and remove stop words.
+
+    Input
+    -----
+    text : str
+        Text to be cleaned.
+    stop_words : list (str)
+        Words to remove from text.
+
+    Output
+    ------
+    text : str
+        Processed text.
     '''
-    
+
     # make text lowercase
-    text = text.lower() 
-    
+    text = text.lower()
+
     # remove mentions
     text = re.sub("(@[A-Za-z0-9]+)", "", text)
-    
+
     # remove links
-    text = re.sub(r'http\S+', '', text) 
+    text = re.sub(r'http\S+', '', text)
     text = re.sub(r'pic\.\S+', '', text)
-    
+
     # convert emoticons
     emoticons = load_dict_emoticons()
     words = text.split()
-    words_edit = [emoticons[word] if word in emoticons else word for word in words]
+    words_edit = [emoticons[word]
+                  if word in emoticons else word for word in words]
     tweet = ' '.join(words_edit)
 
     # convert emojis
     text = emoji.demojize(text)
-    text = text.replace(':', ' ') # separate emojis-words with space
-    
+    text = text.replace(':', ' ')  # separate emojis-words with space
+
     # remove punctuation
-    text = text.replace('...', ' ') # special cases
+    text = text.replace('...', ' ')  # special cases
     text = text.replace('-', ' ')
-    text = text.translate(str.maketrans('', '', '!"$%&*()+,./;<=>?@[\\]^_`{|}~')) 
-    
+    text = text.translate(
+        str.maketrans(
+            '',
+            '',
+            '!"$%&*()+,./;<=>?@[\\]^_`{|}~'))
+
     # tokenize words
     tokenizer = RegexpTokenizer("(#?[a-zA-Z]+[0-9]*(?:'[a-zx]+)?)")
     words = tokenizer.tokenize(text)
-    
+
     # convert contractions
     contractions = load_dict_contractions()
     words = text.split()
-    words_edit = [contractions[word] if word in contractions else word for word in words]
+    words_edit = [contractions[word]
+                  if word in contractions else word for word in words]
     text = ' '.join(words_edit)
 
     # remove stop words and lemmatize
     lemmatizer = WordNetLemmatizer()
     words = tokenizer.tokenize(text)
-    words = [lemmatizer.lemmatize(word) for word in words if word not in stop_words]
+    words = [lemmatizer.lemmatize(word)
+             for word in words if word not in stop_words]
     text = ' '.join(words)
-    
+
     return text
 
 
 def lda_getter(x):
-    
     '''
-    Input a list of tuples containing LDA topic weights.
-    
-    Convert to a dictionary and find the topic number with the highest weight.
-    
-    Output LDA topic number.
+    Function to find the LDA topic number with the highest weight.
+
+    Input
+    -----
+    x : list (tuple)
+        LDA topic weights.
+
+    Output
+    ------
+    topic : int
+        LDA topic number.
     '''
-    
-    # dictionary
+
+    # convert to dictionary
     x_dict = dict(x)
-    
+
     # obtain topic with highest weight and convert to integer
     topic = int(max(x_dict, key=x_dict.get))
-    
+
     return topic
 
 
 def mask_pos_finder(text, word):
-    
     '''
-    Input text as a string and a target word as a string.
-    
-    Find and return the part-of-speech (POS) tag for target word.
-    
-    Output POS tag as a string.
+    Function to find the part-of-speech (POS) tag for a target word.
+
+    Input
+    -----
+    text : str, text to be analyzed
+    word : str, target word in the text
+
+    Output
+    ------
+    tag[1] : str, POS tag
     '''
-    
+
     # analyze text using TextBlob
     pos = TextBlob(text)
-    
+
     # loop over list of POS tags
     for tag in pos.tags:
-        
+
         # find and return POS tag
         if word in tag[0]:
             return tag[1]
-        
-        
+
 # confusion matrix plotter
 def plot_confusion_matrix(cm, classes,
                           normalize=False,
@@ -471,9 +551,27 @@ def plot_confusion_matrix(cm, classes,
                           cmap=plt.cm.Blues):
     
     '''
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    Code modified from work by SeanAbu Wilson.
+    This function prints and plots a model's confusion matrix.
+    
+    Input
+    -----
+    cm : sklearn confusion matrix, 
+        `sklearn.metrics.confusion_matrix(y_true, y_pred)`
+    classes : list, names of target classes 
+    
+    Optional input
+    --------------
+    normalize : bool, whether to apply normalization (default=False)
+        Normalization can be applied by setting `normalize=True`.
+    title : str, title of the returned plot
+    cmap : matplotlib color map
+    
+    Output
+    ------
+    Prints a stylized confusion matrix.
+    
+    
+    [Code modified from work by Sean Abu Wilson.]
     '''
     
     # convert to percentage, if normalize set to True
